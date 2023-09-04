64b33342c1bc46cbee2c8c33ccd8c030d544da62
claitz <stefanomus@fastmail.com>
2023-09-04 19:09:16 +0100

    Removed default values in config

--- next.config.js
+++ next.config.js
@@ -1,36 +1,39 @@
 module.exports = {
+  images: {
+    dangerouslyAllowSVG: true,
+  },
   publicRuntimeConfig: {
     // General settings
-    serverName: process.env.SERVER_NAME || "OpenDAoC",
-    siteUrl: process.env.SITE_URL || "http://localhost:3000",
-    siteTitle: process.env.SITE_TITLE || "OpenDAoC | Account Manager",
-    siteDescription: process.env.SITE_DESCRIPTION || "OpenDAoC Account Manager allows you to link your OpenDAoC account to your Discord for easy password recovery.",
-    siteFooter: process.env.SITE_FOOTER || "OpenDAoC Account Manager",
-    logoPath: process.env.LOGO_PATH || "/logo.svg",
+    serverName: process.env.SERVER_NAME,
+    siteUrl: process.env.SITE_URL,
+    siteTitle: process.env.SITE_TITLE,
+    siteDescription: process.env.SITE_DESCRIPTION,
+    siteFooter: process.env.SITE_FOOTER,
+    logoPath: process.env.LOGO_PATH,
     // Theme settings
     theme: {
-      foreground: process.env.THEME_FOREGROUND || '0, 0, 0',
-      backgroundLight: process.env.THEME_BACKGROUND_LIGHT || '8, 38, 80',
-      backgroundDark: process.env.THEME_BACKGROUND_DARK || '2, 7, 19',
+      foreground: process.env.THEME_FOREGROUND,
+      backgroundLight: process.env.THEME_BACKGROUND_LIGHT,
+      backgroundDark: process.env.THEME_BACKGROUND_DARK,
     },
     
     // Toast notifications settings
     toastDuration: parseInt(process.env.TOAST_DURATION) || 8,
     
     // Database settings
-    DATABASE_HOST: process.env.DATABASE_HOST || 'database-host',
-    DATABASE_USER: process.env.DATABASE_USER || 'database-user',
-    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'database-password',
-    DATABASE_NAME: process.env.DATABASE_NAME || 'database-name',
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_NAME: process.env.DATABASE_NAME,
     
     // Password settings
-    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY || 'encryption-key', // 32 characters, generated with `openssl rand -hex 16`
-    PROHIBITED_CHARACTERS: process.env.PROHIBITED_CHARACTERS?.split(",") || [" ", "#", "&", "%", ".", "!", "^", "_", "-"],
-    MIN_PASSWORD_LENGTH: parseInt(process.env.MIN_PASSWORD_LENGTH) || 6,
-    MAX_PASSWORD_LENGTH: parseInt(process.env.MAX_PASSWORD_LENGTH) || 12,
+    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY, // 32 characters, generated with `openssl rand -hex 16`
+    PROHIBITED_CHARACTERS: process.env.PROHIBITED_CHARACTERS?.split(",") || [" ","#","&","%",".","!","^","_","-"].split(","),
+    MIN_PASSWORD_LENGTH: parseInt(process.env.MIN_PASSWORD_LENGTH),
+    MAX_PASSWORD_LENGTH: parseInt(process.env.MAX_PASSWORD_LENGTH),
     
     // Discord settings
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID || 'my-discord-client-id',
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET || 'my-discord-client-secret',
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   }
 }
